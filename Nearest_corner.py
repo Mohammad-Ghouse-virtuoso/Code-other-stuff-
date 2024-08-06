b5e0c8e455b5eed0e539981f35b10ae07616a6db
Shaik Mohammad Ghouse <84439948+shaikh-cyber@users.noreply.github.com>
2024-08-06 23:20:52 +0530

    Nearest_corner.py
    Test cases 5/5 from PDF page no. 36

---
+++ Nearest_corner.py
@@ -0,0 +1,13 @@
+chair=input()
+s=list(input().split())
+z=999
+c_ind=s.index(chair)
+for i in range(0,c_ind):
+    if s[i]=="-":
+        if abs(c_ind-i)-1<z:
+            z=abs(c_ind-i)-1
+for i in range(c_ind+1,len(s)):
+    if(s[i]=="-"):
+        if abs(i-c_ind)-1<z:
+            z=abs(i-c_ind)-1
+print(z)
