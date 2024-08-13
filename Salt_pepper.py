6f50acdef7f832ef7df2a8ee5fa36a62e76c4d4a
Shaik Mohammad Ghouse <84439948+shaikh-cyber@users.noreply.github.com>
2024-08-13 23:45:48 +0530

    Create Salt_pepper.py
    3
    1 3 5
    2 8 6
    = 11

---
+++ Salt_pepper.py
@@ -0,0 +1,12 @@
+def solve(n, salt, pepper): 
+  s,p=0,0
+  r=[]
+  for i in range(len(salt)): 
+    r.append(salt[i] pepper[i]) 
+  return max(r)
+
+n=int(input()) 
+salt=list(map(int,input().split()))  
+pepper=list(map(int,input().split())) 
+res=solve(n, salt,pepper)
+print(res)
