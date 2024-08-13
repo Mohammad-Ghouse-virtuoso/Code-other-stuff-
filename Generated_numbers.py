f7d6a26d297f516771892d0d13368a6dd17b8939
Shaik Mohammad Ghouse <84439948+shaikh-cyber@users.noreply.github.com>
2024-08-13 23:48:40 +0530

    Create Generated_numbers.py
    10 3 5 
    = 6

---
+++ Generated_numbers.py
@@ -0,0 +1,9 @@
+input1,input2,input3=list(map(int, input().split())) 
+unique_values=set() 
+
+for i in range(input1//input2+1): 
+ for j in range(input1//3+1): 
+    remaining_value-input1-i input2-j input3 
+    if remaining_value>0: 
+      unique_values.add(remaining_value) 
+print(len(unique_values
