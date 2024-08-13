baf32919b3c9fa6b1ddc911a8c174cb51780c049
Shaik Mohammad Ghouse <84439948+shaikh-cyber@users.noreply.github.com>
2024-08-13 23:52:22 +0530

    Create prefix_suffix_balance.py
    5
    1 2 3 4 5
    
    =[13,9,3,5,15]

---
+++ prefix_suffix_balance.py
@@ -0,0 +1,13 @@
+def find absolute differences(N, A): 
+  r=[] 
+  total sum=sum(A) 
+  first part sum for i in range(N): 
+  first_part_sı +=A[i] 
+  second part sum-total sum-first part 
+  sum difference=abs(first_part_sum-second_part_sum) 
+  r.append(difference) 
+  return r
+
+N=int(input()) 
+A=list(map(int, input().split())) 
+print(find absolute differences (N,A)
